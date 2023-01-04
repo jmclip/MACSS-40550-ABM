172685e588c116fb2c5da44841ea193af9a25636
jmclip <41075491+jmclip@users.noreply.github.com>
2023-01-04 15:46:57 -0600

    Restructured topics

--- 7_Analysis_YAAWN/wolf_sheep/test_random_walk.py
+++ 7_Analysis_YAAWN/wolf_sheep/test_random_walk.py
@@ -1,52 +1,52 @@
-'''
+"""
 Testing the RandomWalker by having an ABM composed only of random walker
 agents.
-'''
-
-import random
+"""
 
 from mesa import Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.visualization.TextVisualization import TextVisualization, TextGrid
 
-from random_walk import RandomWalker
+from wolf_sheep.random_walk import RandomWalker
 
 
 class WalkerAgent(RandomWalker):
-    '''
+    """
     Agent which only walks around.
-    '''
+    """
 
     def step(self):
         self.random_move()
 
 
 class WalkerWorld(Model):
-    '''
+    """
     Random walker world.
-    '''
+    """
+
     height = 10
     width = 10
 
-    def __init__(self, height, width, agent_count):
-        '''
+    def __init__(self, width, height, agent_count):
+        """
         Create a new WalkerWorld.
+
         Args:
-            height, width: World size.
+            width, height: World size.
             agent_count: How many agents to create.
-        '''
+        """
         self.height = height
         self.width = width
-        self.grid = MultiGrid(self.height, self.width, torus=True)
+        self.grid = MultiGrid(self.width, self.height, torus=True)
         self.agent_count = agent_count
 
         self.schedule = RandomActivation(self)
         # Create agents
         for i in range(self.agent_count):
-            x = random.randrange(self.width)
-            y = random.randrange(self.height)
-            a = WalkerAgent((x, y), self, True)
+            x = self.random.randrange(self.width)
+            y = self.random.randrange(self.height)
+            a = WalkerAgent(i, (x, y), self, True)
             self.schedule.add(a)
             self.grid.place_agent(a, (x, y))
 
@@ -55,17 +55,18 @@ class WalkerWorld(Model):
 
 
 class WalkerWorldViz(TextVisualization):
-    '''
+    """
     ASCII Visualization for a WalkerWorld agent.
     Each cell is displayed as the number of agents currently in that cell.
-    '''
+    """
 
     def __init__(self, model):
-        '''
+        """
         Create a new visualization for a WalkerWorld instance.
+
         args:
             model: An instance of a WalkerWorld model.
-        '''
+        """
         self.model = model
         grid_viz = TextGrid(self.model.grid, None)
         grid_viz.converter = lambda x: str(len(x))
